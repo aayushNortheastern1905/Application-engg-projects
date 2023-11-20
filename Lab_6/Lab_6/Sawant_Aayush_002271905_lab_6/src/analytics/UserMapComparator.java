43ae3c1915697b89d22d6c79f11161314f320949
aayushNortheastern1905 <sawant.aay@northeastern.edu>
2023-11-19 20:18:57 -0500

    Lab 6 commit

---
+++ Lab_6/Lab_6/Sawant_Aayush_002271905_lab_6/src/analytics/UserMapComparator.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package analytics;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import model.User;
+/**
+ *
+ * @author aayush
+ */
+public class UserMapComparator implements Comparator<User>{
+
+    HashMap<Integer, Integer> userPostMap;
+    
+    public UserMapComparator(HashMap<Integer, Integer> userPostMap) {
+        this.userPostMap = userPostMap;
+    }
+    @Override
+    public int compare(User o1, User o2) {
+        return Integer.compare(userPostMap.get(o1.getId()), userPostMap.get(o2.getId()));
+    
+    }
+    
+}
